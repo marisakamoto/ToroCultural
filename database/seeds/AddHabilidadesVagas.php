e0ef327406be4b71f723d0e942814c8ff4a1c94a
amandamcmolina <56768735+amandamcmolina@users.noreply.github.com>
2020-09-02 10:37:02 -0300

    Código para novas seeds revisado

--- database/seeds/AddHabilidadesVagas.php
+++ database/seeds/AddHabilidadesVagas.php
@@ -14,24 +14,72 @@ class AddHabilidadesVagas extends Seeder
     {
         $now = date('Y-m-d H:i:s');
         DB::table('habilidade_vaga')->insert([
-      [
-            "vaga_id" => 1,
-            "habilidade_id" => 1,
-            "created_at" => $now,
-            "updated_at"=> $now
-      ],
-      [
-            "vaga_id" => 1,
-            "habilidade_id" => 2,
-            "created_at" => $now,
-            "updated_at"=> $now
-      ],
-      [
-            "vaga_id" => 2,
-            "habilidade_id" => 3,
-            "created_at" => $now,
-            "updated_at"=> $now
-      ],
-]);
+            [
+                  "vaga_id" => 1,
+                  "habilidade_id" => 1,
+                  "created_at" => $now,
+                  "updated_at"=> $now
+            ],
+            [
+                  "vaga_id" => 1,
+                  "habilidade_id" => 2,
+                  "created_at" => $now,
+                  "updated_at"=> $now
+            ],
+            [
+                  "vaga_id" => 1,
+                  "habilidade_id" => 5,
+                  "created_at" => $now,
+                  "updated_at"=> $now
+            ],
+            [
+                  "vaga_id" => 1,
+                  "habilidade_id" => 6,
+                  "created_at" => $now,
+                  "updated_at"=> $now
+            ],
+            [
+                  "vaga_id" => 2,
+                  "habilidade_id" => 3,
+                  "created_at" => $now,
+                  "updated_at"=> $now
+            ],
+            [
+                  "vaga_id" => 2,
+                  "habilidade_id" => 4,
+                  "created_at" => $now,
+                  "updated_at"=> $now
+            ],
+            [
+                  "vaga_id" => 3,
+                  "habilidade_id" => 5,
+                  "created_at" => $now,
+                  "updated_at"=> $now
+            ],
+            [
+                  "vaga_id" => 4,
+                  "habilidade_id" => 8,
+                  "created_at" => $now,
+                  "updated_at"=> $now
+            ],
+            [
+                  "vaga_id" => 5,
+                  "habilidade_id" => 7,
+                  "created_at" => $now,
+                  "updated_at"=> $now
+            ],
+            [
+                  "vaga_id" => 5,
+                  "habilidade_id" => 1,
+                  "created_at" => $now,
+                  "updated_at"=> $now
+            ],
+            [
+                  "vaga_id" => 5,
+                  "habilidade_id" => 2,
+                  "created_at" => $now,
+                  "updated_at"=> $now
+            ],
+      ]);
     }
 }
